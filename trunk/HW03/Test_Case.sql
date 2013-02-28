f0b865dec25e511fba44a4b98ce49686ee32ba3d
apoorv.contact@gmail.com <apoorv.contact@gmail.com@d2c8091c-32db-ea61-b059-c57dca84a3ed>
2013-02-28 20:51:35 +0000

    trigger 1-b finished

--- trunk/HW03/Test_Case.sql
+++ trunk/HW03/Test_Case.sql
@@ -4,6 +4,7 @@ use master;
 --part (a)
 
 delete from CLIMBED
+delete from PARTICIPATED
 
 select Count(*) from CLIMBED
 
@@ -13,10 +14,6 @@ INSERT INTO climbed VALUES (23, 'Lion Rock', '08/09/2004');
 INSERT INTO climbed VALUES (23, 'Mount Williamson', '06/09/2004');
 INSERT INTO climbed VALUES (29, 'Lion Rock', '06/09/2004');
 
-
-
-
-
 select min(temp.Closest)
 	from (select abs(datediff(day,WHEN_CLIMBED,'2004-08-09 00:00:00.000')) as "Closest"
 			from climbed 
@@ -24,5 +21,18 @@ select min(temp.Closest)
 			) as temp			
 
 
+-- Test Case 1
+--part (b)
 
+
+delete from CLIMBED
+delete from PARTICIPATED
+
+
+DELETE FROM participated WHERE trip_id = 12; SELECT COUNT(*) FROM climbed WHERE trip_id = 12;
+DELETE FROM participated WHERE trip_id = 13 AND name <> 'ELIZABETH'; SELECT COUNT(*) FROM climbed WHERE trip_id = 13;
+DELETE FROM participated WHERE name = 'ELIZABETH'; SELECT COUNT(*) FROM climbed WHERE trip_id = 13;
+
+SELECT COUNT (DISTINCT trip_id) FROM climbed; DELETE FROM participated WHERE trip_id IN (SELECT trip_id FROM participated WHERE name = 'LINDA'); 
+SELECT COUNT (DISTINCT trip_id) FROM climbed;
 
