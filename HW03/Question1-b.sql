c39da17e7ae76b7582d5c099df42d2d06d3f328b
apoorv.contact@gmail.com <apoorv.contact@gmail.com@d2c8091c-32db-ea61-b059-c57dca84a3ed>
2013-02-28 20:51:35 +0000

    trigger 1-b finished

--- HW03/Question1-b.sql
+++ HW03/Question1-b.sql
@@ -2,3 +2,18 @@
 ---Question 1---
 
 --Trigger 2
+ alter  TRIGGER trigDelParticipated
+ on Participated  
+ after delete
+ as
+ begin
+ 	if @@ROWCOUNT = 0
+		return
+
+	delete 
+	from climbed
+	where TRIP_ID not in (
+							select TRIP_ID
+							from PARTICIPATED	
+							)
+ end
