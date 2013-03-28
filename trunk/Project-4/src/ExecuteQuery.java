7675db5ab9f4298b28718afeb50a085142ec466e
apoorv.contact@gmail.com <apoorv.contact@gmail.com@d2c8091c-32db-ea61-b059-c57dca84a3ed>
2013-03-28 07:23:17 +0000

    working 4.2

--- trunk/Project-4/src/ExecuteQuery.java
+++ trunk/Project-4/src/ExecuteQuery.java
@@ -19,6 +19,9 @@ public class ExecuteQuery {
 	Expression where;
 	Map <String, TableData> res;
 	ArrayList<ResultValue> selTypes;
+	String outputFile;
+	String compiler;
+	String outputLocation;
 	
 	public ExecuteQuery(Map <String, String> myFrom,ArrayList <Expression> mySelect,String att,Expression where,
 			ArrayList<ResultValue> selTypes){
@@ -28,8 +31,33 @@ public class ExecuteQuery {
 		this.where = where;	
 		this.res = Interpreter.res;
 		this.selTypes = selTypes;
+		outputFile= "src/out.tbl";
+		compiler = "g++";
+		outputLocation = "src/cppDir/";
+		
 	}
 	
+	/**
+	 * @param outputFile the outputFile to set
+	 */
+	public void setOutputFile(String outputFile) {
+		this.outputFile = outputFile;
+	}
+
+	/**
+	 * @param compiler the compiler to set
+	 */
+	public void setCompiler(String compiler) {
+		this.compiler = compiler;
+	}
+
+	/**
+	 * @param outputLocation the outputLocation to set
+	 */
+	public void setOutputLocation(String outputLocation) {
+		this.outputLocation = outputLocation;
+	}
+
 	ArrayList <Attribute> getAttributeInfo (String tableName){
 		ArrayList <Attribute> attributes = new ArrayList<Attribute>();
 		Map<String, AttInfo> attributesInfo = res.get(tableName).getAttributes();
@@ -64,11 +92,15 @@ public class ExecuteQuery {
 		return outAttributes;
 	}
 	
-	HashMap <String, String> makeSelectExpression (){
+	HashMap <String, String> makeSelectExpression (ArrayList<Attribute> selectExp){
 		HashMap <String, String> exprs = new HashMap <String, String> ();
-		
-		
-		
+		Iterator<Attribute> attributes = selectExp.iterator();
+		Iterator<Expression> selExprs = mySelect.iterator();
+		while(attributes.hasNext()){
+			String selExpression = selExprs.next().getValue();
+			String param = selExpression.substring(selExpression.indexOf(".")+1);
+			exprs.put(attributes.next().getName(),param);
+		}		
 		return exprs;
 	}
 	
@@ -81,13 +113,23 @@ public class ExecuteQuery {
 	
 	
 	public void doSelection(){
-		ArrayList <Attribute> tableAttribute = null;
+		ArrayList <Attribute> tableAttribute = new ArrayList<Attribute>();
 		Iterator<String> aliases = myFrom.keySet().iterator();
+		String tableName = null;
 		while(aliases.hasNext()){
-			String tableName = myFrom.get(aliases.next().toString());
+			tableName = myFrom.get(aliases.next().toString());
 			tableAttribute = getAttributeInfo(tableName);
 		}
-		Iterator<Attribute> att = makeTypeOutAttributes().iterator();
-		
+		ArrayList <Attribute> selectExpTypes = makeTypeOutAttributes();
+		HashMap <String, String> exprs = makeSelectExpression(selectExpTypes);
+	    String selection = "";
+	    String tableUsed = "src/"+tableName+".tbl";
+	    System.out.println(tableUsed);
+	    // run the selection operation
+	    try {
+	      Selection foo = new Selection (tableAttribute, selectExpTypes, selection, exprs, tableUsed, outputFile, compiler, outputLocation ); 
+	    } catch (Exception e) {
+	      throw new RuntimeException (e);
+	    }
 	}
 }
