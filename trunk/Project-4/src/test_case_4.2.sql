07e2ac28c56987646ce36070081a98bc652b3fe4
apoorv.contact@gmail.com <apoorv.contact@gmail.com@d2c8091c-32db-ea61-b059-c57dca84a3ed>
2013-03-29 03:33:36 +0000

    select query worked with where clause

--- trunk/Project-4/src/test_case_4.2.sql
+++ trunk/Project-4/src/test_case_4.2.sql
@@ -1,5 +1,34 @@
- SELECT
+ __________________________________________________
+
+ SELECT	
+ 		c.c_custkey,
 		c.c_phone,
-		c.c_custkey
+		c.c_acctbal		
  FROM
 		customer AS c;
+__________________________________________________
+
+ SELECT	
+ 		c1.c_custkey,
+		c1.c_phone,
+		c1.c_acctbal		
+ FROM
+		customer AS c1
+
+  WHERE
+  		c1.c_custkey > 10 ;		
+__________________________________________________
+
+ SELECT	
+ 		c1.c_custkey,
+		c1.c_phone,
+		c1.c_acctbal		
+ FROM
+		customer AS c1,
+		customer As c2
+
+  WHERE
+  		c1.c_custkey > 10 and c2.c_custkey;
+
+__________________________________________________
+
