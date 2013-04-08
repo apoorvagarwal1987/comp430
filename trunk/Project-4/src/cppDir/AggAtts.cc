677c396051bc49a63f4090020c292a3fde88208c
apoorv.contact@gmail.com <apoorv.contact@gmail.com@d2c8091c-32db-ea61-b059-c57dca84a3ed>
2013-04-08 23:32:38 +0000

    removed unwanted file

--- trunk/Project-4/src/cppDir/AggAtts.cc
+++
@@ -1,7 +0,0 @@
-Str ccccccc_o_orderdate;
-Str ccccccc_o_orderstatus;
-Str att1;
-Str att2;
-Float att3;
-Int att4;
-Int ccccccc_count;
--- trunk/Project-4/src/cppDir/WriteOut.cc
+++
@@ -1,5 +0,0 @@
-Str att1;
-
-att1 = Str ("supplier name was ") + s_s_name;
-
-att1.WriteOut (toMe);
