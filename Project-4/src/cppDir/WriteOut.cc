e6e16201f5063f99fc4b6c2c12aa2b4c13cfb3a4
apoorv.contact@gmail.com <apoorv.contact@gmail.com@d2c8091c-32db-ea61-b059-c57dca84a3ed>
2013-04-08 23:31:30 +0000

    removed unwanted file

--- Project-4/src/cppDir/WriteOut.cc
+++ Project-4/src/cppDir/WriteOut.cc
@@ -1,11 +1,5 @@
-Int att1;
-Str att2;
-Str att3;
+Str att1;
 
-att1 = r1_r_regionkey;
-att2 = r1_r_name;
-att3 = r2_r_name;
+att1 = Str ("supplier name was ") + s_s_name;
 
 att1.WriteOut (toMe);
-att2.WriteOut (toMe);
-att3.WriteOut (toMe);
