639464ca9e81743d0439db2c097b71dab3f3d892
apoorv.contact@gmail.com <apoorv.contact@gmail.com@d2c8091c-32db-ea61-b059-c57dca84a3ed>
2013-04-09 01:19:46 +0000

    ADDED SOME FILE

---
+++ trunk/Project-4/src/cppDir/Atts.cc
@@ -0,0 +1,16 @@
+Int l1_l_orderkey;
+Int l1_l_partkey;
+Int l1_l_suppkey;
+Int l1_l_linenumber;
+Int l1_l_quantity;
+Float l1_l_extendedprice;
+Float l1_l_discount;
+Float l1_l_tax;
+Str l1_l_returnflag;
+Str l1_l_linestatus;
+Str l1_l_shipdate;
+Str l1_l_commitdate;
+Str l1_l_receiptdate;
+Str l1_l_shipinstruct;
+Str l1_l_shipmode;
+Str l1_l_comment;
