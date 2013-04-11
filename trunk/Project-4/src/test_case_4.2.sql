770f8562665bb33d36a676cb90cdcabafacc667c
apoorv.contact@gmail.com <apoorv.contact@gmail.com@d2c8091c-32db-ea61-b059-c57dca84a3ed>
2013-04-11 06:34:54 +0000

    test queries

--- trunk/Project-4/src/test_case_4.2.sql
+++ trunk/Project-4/src/test_case_4.2.sql
@@ -222,6 +222,7 @@ WHERE
     (s1.s_nationkey = 19) and
     (o1.o_custkey = 5);
 __________________________________________________
+
 select
     "supplier name was " + s.s_name
 from
@@ -275,3 +276,133 @@ where
     and (not l2.l_suppkey = l1.l_suppkey);
 
 __________________________________________________
+
+
+SELECT 
+    r1.r_regionkey,
+    n1.n_regionkey,
+    s1.s_nationkey    
+FROM 
+    region AS r1,
+    nation AS n1,
+    supplier AS s1,
+    part AS p1,
+    orders AS o1
+WHERE 
+    (r1.r_regionkey = n1.n_regionkey OR p1.p_partkey = 12) and 
+    (n1.n_nationkey = s1.s_nationkey) and 
+    (s1.s_nationkey = 19) and
+    (o1.o_custkey = 5);
+
+__________________________________________________
+
+
+select 
+    sum (1), 
+    avg ((o.o_totalprice - 32592.14) / 32592.14)
+from
+    orders as o
+where
+    (o.o_orderstatus = "F") and 
+    (o.o_orderpriority < "2-HIGH" or o.o_orderpriority = "2-HIGH");
+
+__________________________________________________
+
+select 
+    o.o_orderstatus, 
+    o.o_orderpriority,
+    o.o_totalprice
+from
+    orders as o
+where
+    (o.o_orderstatus = "F") and 
+    (o.o_orderpriority < "2-HIGH" or o.o_orderpriority = "2-HIGH");
+
+__________________________________________________
+
+
+select 
+    "status :" + o.o_orderstatus,
+    "data: " + o.o_orderdate,
+    avg(o.o_totalprice * 100),
+    sum(1)
+from
+    orders as o
+where
+    (o.o_orderstatus = "F")
+group by
+    o.o_orderdate;
+
+
+__________________________________________________
+
+
+select
+    p2.ps_partkey, 
+    avg (p2.ps_supplycost)
+from
+    part as p1,
+    partsupp as p2,
+    supplier as s,
+    nation as n,
+    region as r
+where
+    (p1.p_partkey = p2.ps_partkey)
+    and (s.s_suppkey = p2.ps_suppkey)
+    and (s.s_nationkey = n.n_nationkey)
+    and (n.n_regionkey = r.r_regionkey)
+    and (r.r_name = "AMERICA")
+group by
+    p2.ps_partkey;
+
+  4763 milliseconds  w/o sel down
+  8228 milliseconds  w sel down
+_______________________________________________
+
+
+select
+    n.n_name,
+    o.o_orderdate,
+    l.l_extendedprice * (1 - l.l_discount) - p2.ps_supplycost * l.l_quantity 
+from
+    part as p1,
+    supplier as s,
+    lineitem as l,
+    partsupp as p2,
+    orders as o,
+    nation as n
+where
+    (s.s_suppkey = l.l_suppkey)
+    and (p2.ps_suppkey = l.l_suppkey)
+    and (p2.ps_partkey = l.l_partkey)
+    and (p1.p_partkey = l.l_partkey)
+    and (o.o_orderkey = l.l_orderkey)
+    and (s.s_nationkey = n.n_nationkey)
+    and (p1.p_type = "STANDARD POLISHED TIN" or
+         p1.p_type = "ECONOMY BRUSHED TIN" or
+         p1.p_type = "ECONOMY POLISHED NICKEL" or
+         p1.p_type = "SMALL ANODIZED COPPER");
+
+
+__________________________________________________
+
+select
+	l1.l_shipmode,
+	sum (1)
+from
+	orders as o1,
+	lineitem as l1
+where
+	(o1.o_orderkey = l1.l_orderkey)
+	and (o1.o_orderstatus = "F")
+	and (l1.l_shipmode = "truck" or l1.l_shipmode = "rail")
+	and (l1.l_commitdate < l1.l_receiptdate)
+	and (l1.l_shipdate < l1.l_commitdate)
+	and (l1.l_receiptdate > "1996-06-20" or l1.l_receiptdate = "1996-06-20")
+	and (l1.l_receiptdate < "1997-06-20" or l1.l_receiptdate = "1997-06-20")
+group by
+	l1.l_shipmode;
+
+_________________________________________________________
+
+_______________________________________________
