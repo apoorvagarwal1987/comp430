8fa986f184f6321db7a742c6eb08b7bbfe06e102
apoorv.contact@gmail.com <apoorv.contact@gmail.com@d2c8091c-32db-ea61-b059-c57dca84a3ed>
2013-04-16 07:57:23 +0000

    still working on the costing

--- Project-4/src/TableOrderComparator.java
+++ Project-4/src/TableOrderComparator.java
@@ -29,9 +29,9 @@ public class TableOrderComparator implements Comparator<RATableType> {
 	*/
 	
 	public int compare(RATableType o1, RATableType o2) {
-		if(o1.getTupleCount() > o2.getTupleCount())
+		if(o1.getjoinPriority() > o2.getjoinPriority())
 				return 1;
-			else if (o1.getTupleCount() == o2.getTupleCount())
+			else if (o1.getjoinPriority() == o2.getjoinPriority())
 				return 0;
 			else
 				return -1;
